c62d76001598b0c35ce6ce27fba081163db7ae6b
dylon <hustyugm@gmail.com>
2016-12-21 11:54:38 +0800

    update

--- src/main/scala/com/aliyun/emr/example/RunLocally.scala
+++ src/main/scala/com/aliyun/emr/example/RunLocally.scala
@@ -23,7 +23,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 trait RunLocally {
   val conf = new SparkConf().setAppName(getAppName).setMaster("local[4]")
   conf.set("spark.hadoop.fs.oss.impl", "com.aliyun.fs.oss.nat.NativeOssFileSystem")
-  conf.set("spark.hadoop.job.runlocal", "true")
+  conf.set("spark.hadoop.mapreduce.job.run-local", "true")
   val sc = new SparkContext(conf)
 
   def getAppName: String
